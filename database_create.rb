97b16aa3bb3ea0c58efa83137439ab2f6129b8ba
Mike Goren <mgoren@mortalwombat.net>
2015-01-23 16:56:12 -0800

    update database creation script

--- database_create.rb
+++ database_create.rb
@@ -1,11 +1,12 @@
+# for this example
+
+
 require('pg')
 
-DB = PG.connect({:dbname => "mgoren"})
+DB_tmp = PG.connect({:dbname => "postgres"}) # change postgres to your default db name if postgres does not exist
 
 db_name = "salon"
 db_name_test = (db_name + "_test")
-###############################################
-
 
 table1_name = "stylists"
 table1_column1 = "name"
@@ -17,22 +18,13 @@ table2_column1_type = "varchar"
 table2_column2 = "stylist_id"
 table2_column2_type = "int"
 
+DB_tmp.exec("CREATE DATABASE #{db_name};")
 
-DB.exec("CREATE DATABASE #{db_name};")
+DB = PG.connect({:dbname => db_name})
+DB.exec("CREATE TABLE #{table1_name} (id serial PRIMARY KEY, #{table1_column1} #{table1_column1_type});")
+DB.exec("CREATE TABLE #{table2_name} (id serial PRIMARY KEY, #{table2_column1} #{table2_column1_type}, #{table2_column2} #{table2_column2_type});")
 
-#disconnect from Guest database and connect to our new one
-DB2 = PG.connect({:dbname => db_name})
-DB2.exec("CREATE TABLE #{table1_name} (id serial PRIMARY KEY, #{table1_column1} #{table1_column1_type});")
-DB2.exec("CREATE TABLE #{table2_name} (id serial PRIMARY KEY, #{table2_column1} #{table2_column1_type}, #{table2_column2} #{table2_column2_type});")
+DB.exec("CREATE DATABASE #{db_name_test} WITH TEMPLATE #{db_name};")
 
-#create our test database
-DB2.exec("CREATE DATABASE #{db_name_test} WITH TEMPLATE #{db_name};")
-
-###############################################
-
-#or comment out all between the two lines of pound signs above, and delete some databases
-# DB.exec("DROP DATABASE #{db_name};")
-# DB.exec("DROP DATABASE #{db_name_test};")
-
-
-# <3 Dan 2015 danwright.co
+# script by Dan Wright, with modificationsby Mike Goren
+# original script: https://github.com/dwright213/setter_upper
